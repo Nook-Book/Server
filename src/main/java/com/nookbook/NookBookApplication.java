45f4398eff60b0087d0f4c2eadef9a63e05e6eaa
EunbeenDev <ros4312@naver.com>
2024-05-18 14:41:57 +0900

    initial commit

---
+++ src/main/java/com/nookbook/NookBookApplication.java
@@ -0,0 +1,13 @@
+package com.nookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NookBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NookBookApplication.class, args);
+    }
+
+}
