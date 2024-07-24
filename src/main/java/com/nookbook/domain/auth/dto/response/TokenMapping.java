54a6cbd251359460d8d418c6d6d95848a64422a6
EunbeenDev <ros4312@naver.com>
2024-07-24 18:34:15 +0900

    [FIX] Token 칼럼명 수정

--- src/main/java/com/nookbook/domain/auth/dto/response/TokenMapping.java
+++ src/main/java/com/nookbook/domain/auth/dto/response/TokenMapping.java
@@ -1,18 +1,13 @@
 package com.nookbook.domain.auth.dto.response;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class TokenMapping {
-    private String userEmail;
+    private String email;
     private String accessToken;
     private String refreshToken;
-
-    @Builder
-    public TokenMapping(String userEmail, String accessToken, String refreshToken){
-        this.userEmail = userEmail;
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
 }
