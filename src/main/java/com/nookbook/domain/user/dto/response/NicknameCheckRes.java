7bc08b9db5e5df3598214deb37e66bbc8d3225a8
EunbeenDev <ros4312@naver.com>
2024-07-30 14:26:38 +0900

    [FIX] 닉네임/아이디 중복 여부 판단 API 응답 수정

--- src/main/java/com/nookbook/domain/user/dto/response/NicknameCheckRes.java
+++ src/main/java/com/nookbook/domain/user/dto/response/NicknameCheckRes.java
@@ -1,9 +1,10 @@
 package com.nookbook.domain.user.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.*;
 
-
+@Tag(name = "닉네임 중복 체크 응답", description = "사용자의 닉네임이 중복인지 여부를 반환합니다.")
 @Getter
 @Setter
 @Builder
