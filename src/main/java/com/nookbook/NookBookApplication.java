6d0a2c3183d6da903356a7294da4176e831e692d
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2024-08-01 05:20:57 +0900

    [FEAT] S3 설정 추가

--- src/main/java/com/nookbook/NookBookApplication.java
+++ src/main/java/com/nookbook/NookBookApplication.java
@@ -12,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @PropertySource(value = { "classpath:webclient/application-webclient.yml" }, factory = YamlPropertySourceFactory.class)
 @PropertySource(value = { "classpath:swagger/application-springdoc.yml" }, factory = YamlPropertySourceFactory.class)
 @PropertySource(value = { "classpath:oauth2/application-oauth2.yml" }, factory = YamlPropertySourceFactory.class)
+@PropertySource(value = { "classpath:s3/application-s3.yml" }, factory = YamlPropertySourceFactory.class)
 public class NookBookApplication {
 
     public static void main(String[] args) {
