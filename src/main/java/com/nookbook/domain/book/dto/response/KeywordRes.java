2de808e2d8aecab5a5c77b510a4bdc923e9aa5d3
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2024-08-07 02:39:11 +0900

    [DOCS] 스웨거 명세 추가

--- src/main/java/com/nookbook/domain/book/dto/response/KeywordRes.java
+++ src/main/java/com/nookbook/domain/book/dto/response/KeywordRes.java
@@ -1,5 +1,6 @@
 package com.nookbook.domain.book.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class KeywordRes {
+
+    @Schema(type = "Long", example = "1", description = "검색어의 고유 id")
     private Long keywordId;
+
+    @Schema(type = "String", example = "세이노", description = "검색어")
     private String content;
 }
