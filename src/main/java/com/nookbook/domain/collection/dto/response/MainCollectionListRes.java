0b0481fc30af48623fb31c49e09fb075672169a9
EunbeenDev <ros4312@naver.com>
2024-09-02 19:14:19 +0900

    [DOCS] 스웨거 명세 수정

--- src/main/java/com/nookbook/domain/collection/dto/response/MainCollectionListRes.java
+++ src/main/java/com/nookbook/domain/collection/dto/response/MainCollectionListRes.java
@@ -18,7 +18,7 @@ public class MainCollectionListRes {
     @Schema(type = "int", example = "3", description = "현재 컬렉션 수(1개 이상 4개 이하)")
     private int totalCollections;
 
-    @Schema(type = "List", example = "[1,5,3,1]", description = "컬렉션 ID 리스트 (정렬된 상태)")
+    @Schema(type = "List", example = "[1,3,1]", description = "컬렉션 ID 리스트 (정렬된 상태)")
     private ArrayList<Long> collectionIdList;
 
 }
