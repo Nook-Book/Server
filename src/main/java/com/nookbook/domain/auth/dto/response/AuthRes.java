5db9dde3a58e3b3f1d5aaeb232a04a0f61699c8b
EunbeenDev <ros4312@naver.com>
2024-11-28 14:13:32 +0900

    [FEAT] 카카오 로그인, 로그아웃, 탈퇴하기 기능 구현

--- src/main/java/com/nookbook/domain/auth/dto/response/AuthRes.java
+++ src/main/java/com/nookbook/domain/auth/dto/response/AuthRes.java
@@ -1,26 +1,25 @@
 package com.nookbook.domain.auth.dto.response;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class AuthRes {
 
-    @Schema( type = "string", example = "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2NTI3OTgxOTh9.6CoxHB_siOuz6PxsxHYQCgUT1_QbdyKTUwStQDutEd1-cIIARbQ0cyrnAmpIgi3IBoLRaqK7N1vXO42nYy4g5g" , description="access token 을 출력합니다.")
+    @Schema(type = "string", example = "ya29.a0AfH6SMC...", description = "Google Access Token입니다.")
+    @JsonProperty("accessToken")
     private String accessToken;
 
-    @Schema( type = "string", example = "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2NTI3OTgxOTh9.asdf8as4df865as4dfasdf65_asdfweioufsdoiuf_432jdsaFEWFSDV_sadf" , description="refresh token 을 출력합니다.")
+    @Schema(type = "string", example = "string", description = "Refresh Token입니다.")
+    @JsonProperty("refreshToken")
     private String refreshToken;
 
-    @Schema( type = "string", example ="Bearer", description="권한(Authorization) 값 해더의 명칭을 지정합니다.")
-    private String tokenType = "Bearer";
-
-    @Builder
-    public AuthRes(String accessToken, String refreshToken) {
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
-
+    @Schema(type = "string", example = "string@aa.bb", description = "유저 이메일입니다.")
+    @JsonProperty("email")
+    private String email;
 }
