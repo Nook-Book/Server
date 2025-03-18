9407fd349d7d5f051d91f6d022ec2d1274341dee
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2025-03-19 04:59:54 +0900

    [FEAT] spring batch 및 quartz 설정 세팅

--- src/main/java/com/nookbook/NookBookApplication.java
+++ src/main/java/com/nookbook/NookBookApplication.java
@@ -1,6 +1,7 @@
 package com.nookbook;
 
 import com.nookbook.global.config.YamlPropertySourceFactory;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -10,6 +11,7 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableBatchProcessing
 @PropertySource(value = { "classpath:database/application-database.yml" }, factory = YamlPropertySourceFactory.class)
 @PropertySource(value = { "classpath:webclient/application-webclient.yml" }, factory = YamlPropertySourceFactory.class)
 @PropertySource(value = { "classpath:swagger/application-springdoc.yml" }, factory = YamlPropertySourceFactory.class)
