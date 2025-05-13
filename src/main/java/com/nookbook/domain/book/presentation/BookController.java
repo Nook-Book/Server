14b669103bc08a975eac0485578d200aede329b3
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2025-05-13 23:04:39 +0900

    [REFACTOR] Book URL 구조 변경 및 키워드, 타이머와 컨트롤러 분리

--- src/main/java/com/nookbook/domain/book/presentation/BookController.java
+++ src/main/java/com/nookbook/domain/book/presentation/BookController.java
@@ -2,18 +2,12 @@ package com.nookbook.domain.book.presentation;
 
 import com.nookbook.domain.book.applicaiton.BookService;
 import com.nookbook.domain.book.dto.response.*;
-import com.nookbook.domain.keyword.application.KeywordService;
-import com.nookbook.domain.timer.application.TimerService;
-import com.nookbook.domain.timer.dto.request.CreateTimerReq;
-import com.nookbook.domain.timer.dto.response.TimerRes;
 import com.nookbook.domain.user_book.domain.BookStatus;
 import com.nookbook.global.config.security.token.CurrentUser;
 import com.nookbook.global.config.security.token.UserPrincipal;
 import com.nookbook.global.payload.ErrorResponse;
-import com.nookbook.global.payload.Message;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -23,17 +17,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalTime;
-
 @Tag(name = "Book", description = "Book API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/book")
+@RequestMapping("/api/v1/books")
 public class BookController {
 
     private final BookService bookService;
-    private final KeywordService keywordService;
-    private final TimerService timerService;
 
     @Operation(summary = "도서 검색", description = "도서를 제목, 저자, 출판사로 검색합니다.")
     @ApiResponses(value = {
@@ -54,7 +44,7 @@ public class BookController {
             @ApiResponse(responseCode = "200", description = "조회 성공", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = BookRes.class) ) } ),
             @ApiResponse(responseCode = "400", description = "조회 실패", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class) ) } ),
     } )
-    @GetMapping
+    @GetMapping("")
     public ResponseEntity<?> findBookDetail(
             @Parameter(description = "Accesstoken을 입력해주세요.", required = true) @CurrentUser UserPrincipal userPrincipal,
             @Parameter(description = "조회하려는 도서의 isbn을 입력해주세요.", required = true) @RequestParam String isbn
@@ -67,7 +57,7 @@ public class BookController {
             @ApiResponse(responseCode = "200", description = "변경 성공", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = BookStatus.class) ) } ),
             @ApiResponse(responseCode = "400", description = "변경 실패", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class) ) } ),
     } )
-    @PatchMapping("/{bookId}")
+    @PatchMapping("/{bookId}/status")
     public ResponseEntity<?> updateBookStatus(
             @Parameter(description = "Accesstoken을 입력해주세요.", required = true) @CurrentUser UserPrincipal userPrincipal,
             @Parameter(description = "상태를 변경하려는 도서의 id를 입력해주세요.", required = true) @PathVariable Long bookId
@@ -80,7 +70,7 @@ public class BookController {
             @ApiResponse(responseCode = "200", description = "검색 성공", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = BestSellerRes.class) ) } ),
             @ApiResponse(responseCode = "400", description = "검색 실패", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class) ) } ),
     } )
-    @GetMapping("/best-seller")
+    @GetMapping("/best-sellers")
     public ResponseEntity<?> findBestSellers(
             @Parameter(description = "Accesstoken을 입력해주세요.", required = true) @CurrentUser UserPrincipal userPrincipal,
             @Parameter(description = "베스트셀러의 카테고리를 입력해주세요. 종합(0), 소설(1), 경제/경영(170), 자기계발(336), 시(50940), 에세이(55889), 인문/교양(656), 취미/실용(55890), 매거진(2913), 기본값은 종합(0)입니다.", required = true) @RequestParam(defaultValue = "0") int category,
@@ -88,73 +78,5 @@ public class BookController {
             @Parameter(description = "베스트셀러를 페이지별로 조회합니다. **Page는 1부터 시작합니다!**", required = true) @RequestParam(defaultValue = "1") int page
     ) {
         return bookService.getBestSellerByCategory(page, category, size);
-    }
-
-
-    // 검색어 관련
-    @Operation(summary = "검색어 조회", description = "사용자의 검색어를 최대 5개 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "조회 성공", content = { @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = KeywordRes.class))) } ),
-            @ApiResponse(responseCode = "400", description = "조회 실패", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class) ) } ),
-    } )
-    @GetMapping("/keyword")
-    public ResponseEntity<?> findKeywords(
-            @Parameter(description = "Accesstoken을 입력해주세요.", required = true) @CurrentUser UserPrincipal userPrincipal
-    ) {
-        return keywordService.getKeywords(userPrincipal);
-    }
-
-    @Operation(summary = "검색어 삭제", description = "사용자의 검색어를 삭제합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "검색 성공", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class)) } ),
-            @ApiResponse(responseCode = "400", description = "검색 실패", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class) ) } ),
-    } )
-    @DeleteMapping("/keyword/{keywordId}")
-    public ResponseEntity<?> deleteKeyword(
-            @Parameter(description = "Accesstoken을 입력해주세요.", required = true) @CurrentUser UserPrincipal userPrincipal,
-            @Parameter(description = "검색어의 id를 입력해주세요.", required = true) @PathVariable Long keywordId
-    ) {
-        return keywordService.deleteKeyword(userPrincipal, keywordId);
-    }
-
-    @Operation(summary = "타이머 기록 조회", description = "타이머 기록의 누적 시간 및 목록을 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "조회 성공", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = TimerRes.class)) } ),
-            @ApiResponse(responseCode = "400", description = "조회 실패", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class) ) } ),
-    } )
-    @GetMapping("/{bookId}/timer")
-    public ResponseEntity<?> getTimerRecord(
-            @Parameter(description = "Accesstoken을 입력해주세요.", required = true) @CurrentUser UserPrincipal userPrincipal,
-            @Parameter(description = "도서의 id를 입력해주세요.", required = true) @PathVariable Long bookId
-    ) {
-        return timerService.getTimerRecords(userPrincipal, bookId);
-    }
-
-    @Operation(summary = "타이머 종료", description = "타이머를 종료하고, 타이머 기록을 저장합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "저장 성공", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = TimerRes.class)) } ),
-            @ApiResponse(responseCode = "400", description = "저장 실패", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class) ) } ),
-    } )
-    @PostMapping("/{bookId}/timer/{timerId}")
-    public ResponseEntity<?> saveTimerRecord(
-            @Parameter(description = "Accesstoken을 입력해주세요.", required = true) @CurrentUser UserPrincipal userPrincipal,
-            @Parameter(description = "도서의 id를 입력해주세요.", required = true) @PathVariable Long bookId,
-            @Parameter(description = "타이머의 id를 입력해주세요.", required = true) @PathVariable Long timerId,
-            @Parameter(description = "시간을 입력해주세요.", required = true) @RequestBody CreateTimerReq createTimerReq
-            ) {
-        return timerService.saveTimerRecord(userPrincipal, bookId, timerId, createTimerReq);
-    }
-
-    @Operation(summary = "타이머 시작", description = "타이머를 시작합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "저장 성공", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)) } ),
-            @ApiResponse(responseCode = "400", description = "저장 실패", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class) ) } ),
-    } )
-    @PostMapping("/{bookId}/timer")
-    public ResponseEntity<?> startTimerRecord(
-            @Parameter(description = "Accesstoken을 입력해주세요.", required = true) @CurrentUser UserPrincipal userPrincipal,
-            @Parameter(description = "도서의 id를 입력해주세요.", required = true) @PathVariable Long bookId
-    ) {
-        return timerService.updateTimerStatus(userPrincipal, bookId);
     }
 }
